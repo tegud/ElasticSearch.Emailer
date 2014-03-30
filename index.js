bc1d4b2eb1ca32a53d5a988a1f1403d9e5683186
Steve Elliott <steve@tegud.net>
2014-03-30 21:21:54 +0100

    Refactorings, enabling csv output

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var json2csv = require('json2csv');
 var fs = require('fs');
 var _ = require('lodash');
 var async = require('async');
@@ -7,34 +8,7 @@ var client = new elasticsearch.Client({
 	host: '10.44.35.21:9200'
 });
 
-function parseHotels(hotels, callback) {
-	var hotelCount = 0;
-	var productCount = 0;
-
-	async.map(hotels, 
-		function(hotel, callback) {
-			hotelCount++;
-
-			return async.map(hotel.ProductID.buckets, function(product, callback) {
-				productCount++;
-
-				callback(null, {
-					hotel: hotel.key,
-					product: product.key,
-					instances: product.doc_count
-				});
-			}, callback);
-		},
-		function(err, data) {
-			callback(err, {
-				data: data,
-				hotelCount: hotelCount,
-				productCount: productCount
-			});
-		});
-}
-
-client.search({
+var aggregateSearch = {
 	index: 'logstash-2014.03.28',
 	body: {
 	    "aggregations": {
@@ -53,22 +27,80 @@ client.search({
 	            }
 	        }
 	    }
+	}
+};
+
+function parseProducts(products, callback) {
+	async.map(products, function(product, callback) {
+		callback(null, {
+			id: product.key,
+			instances: product.doc_count
+		});
+	}, callback);	
 }
-}).then(function (body) {
-	var hotelCount = 0;
+
+function parseHotels(hotels, callback) {
 	var productCount = 0;
 
-	parseHotels(body.aggregations.HotelID.buckets, function(err, data) {
-		fs.writeFile(__dirname + '/test.json', JSON.stringify(data.data, null, 4), function (err) {
-			console.log(data.hotelCount + ' unique missing hotels found');				
-			console.log(data.productCount + ' unique missing products found');				
+	async.map(hotels, 
+		function(hotel, callback) {
+			productCount += hotel.ProductID.buckets.length;
 
-			console.log('Output written to test.json');
-
-			process.exit(0);
+			parseProducts(hotel.ProductID.buckets, function(err, missingProducts) {
+				callback(err, {
+					id: hotel.key,
+					products: missingProducts
+				})
+			});
+		},
+		function(err, data) {
+			callback(err, {
+				data: data,
+				hotelCount: hotels.length,
+				productCount: productCount
+			});
 		});
+}
+
+var fileWriters = {
+	json: function(data, callback) {
+		fs.writeFile(__dirname + '/output/test.json', JSON.stringify(data, null, 4), callback);
+	},
+	csv: function(data, callback) {
+		var rows = [];
+
+		async.each(data, function(hotel, callback) {
+			async.each(hotel.products, function(product, callback) {
+				rows.push({
+					hotel: hotel.id,
+					product: product.id,
+					instances: product.instances
+				})
+				callback();
+			}, callback);
+		}, function() {
+			json2csv({data: rows, fields: ['hotel', 'product', 'instances']}, function(err, csv) {
+				fs.writeFile(__dirname + '/output/test.csv', csv, callback);
+			});
+		});
+	}
+};
+
+client
+	.search(aggregateSearch)
+	.then(function (body) {
+		parseHotels(body.aggregations.HotelID.buckets, function(err, data) {
+			fileWriters['csv'](data.data, function (err) {
+				console.log(data.hotelCount + ' unique missing hotels found');				
+				console.log(data.productCount + ' unique missing products found');				
+
+				console.log('Output written to test.json');
+
+				process.exit(0);
+			});
+		});
+	}, 
+	function (error) {
+		console.trace(error.message);
+		process.exit(0);
 	});
-}, function (error) {
-	console.trace(error.message);
-	process.exit(0);
-});
