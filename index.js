1f03fb22dec7571b510edb3340100f95cf6ffa58
Steve Elliott <steve@tegud.net>
2014-03-30 20:19:30 +0100

    Refactoring for readability

--- index.js
+++ index.js
@@ -7,6 +7,33 @@ var client = new elasticsearch.Client({
 	host: '10.44.35.21:9200'
 });
 
+function parseHotels(hotels, callback) {
+	var hotelCount = 0;
+	var productCount = 0;
+
+	async.map(hotels, 
+		function(hotel, callback) {
+			hotelCount++;
+
+			return async.map(hotel.ProductID.buckets, function(product, callback) {
+				productCount++;
+
+				callback(null, {
+					hotel: hotel.key,
+					product: product.key,
+					instances: product.doc_count
+				});
+			}, callback);
+		},
+		function(err, data) {
+			callback(err, {
+				data: data,
+				hotelCount: hotelCount,
+				productCount: productCount
+			});
+		});
+}
+
 client.search({
 	index: 'logstash-2014.03.28',
 	body: {
@@ -28,23 +55,20 @@ client.search({
 	    }
 }
 }).then(function (body) {
-	async.map(body.aggregations.HotelID.buckets, 
-		function(hotel, callback) {
-			return async.map(hotel.ProductID.buckets, function(product, callback) {
-				callback(null, {
-					hotel: hotel.key,
-					product: product.key,
-					instances: product.doc_count
-				});
-			}, callback);
-		},
-		function(err, data) {
-			console.log(JSON.stringify(data, null, 4));
-			fs.writeFile(__dirname + '/test.json', JSON.stringify(body, null, 4), function (err) {
-				console.log('Output written to test.json');
-				process.exit(0);
-			});
+	var hotelCount = 0;
+	var productCount = 0;
+
+	parseHotels(body.aggregations.HotelID.buckets, function(err, data) {
+		fs.writeFile(__dirname + '/test.json', JSON.stringify(data.data, null, 4), function (err) {
+			console.log(data.hotelCount + ' unique missing hotels found');				
+			console.log(data.productCount + ' unique missing products found');				
+
+			console.log('Output written to test.json');
+
+			process.exit(0);
 		});
+	});
 }, function (error) {
 	console.trace(error.message);
+	process.exit(0);
 });
